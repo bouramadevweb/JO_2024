3087f0b0def9cd71e5f0da01379041496f4c5efd
Bourama coulibaly <bouramadevweb@gmail.com>
2024-03-17 14:28:27 +0100

    jeux Olympique

---
+++ JeuxOlympique/JeuxOlympique/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for JeuxOlympique project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JeuxOlympique.settings')
+
+application = get_wsgi_application()
