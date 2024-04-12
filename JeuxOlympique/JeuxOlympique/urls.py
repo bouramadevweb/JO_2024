bbb18a8820dab0b5ad8000e6f6be375362718155
Bourama coulibaly <bouramadevweb@gmail.com>
2024-04-12 19:23:12 +0200

    img font nav

--- JeuxOlympique/JeuxOlympique/urls.py
+++ JeuxOlympique/JeuxOlympique/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
+from django.conf import settings
 
-# from .import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('my_app_jo.urls')),
     path('administration/', include('administration.urls')),
    
- ]
-# + static(settings.MEDIA_URL,Document_root=settings.MEDIA_ROOT)
+ ] + static(settings.MEDIA_URL,Document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
