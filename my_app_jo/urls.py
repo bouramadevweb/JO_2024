29449dacc59e25f7017e4745922b7c1cec348414
Bourama coulibaly <bouramadevweb@gmail.com>
2024-04-24 14:06:13 +0200

    url admin

--- my_app_jo/urls.py
+++ my_app_jo/urls.py
@@ -12,7 +12,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='home', permanent=False)),
     path('', views.home, name='home'),
     path('choisir_ticket/', views.choisir_ticket, name='choisir_ticket'),
     path('ajouter_au_panier/',views.ajouter_au_panier,name='ajouter_au_panier'),
