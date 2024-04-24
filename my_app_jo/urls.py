7924132a64ca6e44254b60961739871e12a1a9aa
Bourama coulibaly <bouramadevweb@gmail.com>
2024-04-24 11:34:17 +0200

    test url

--- my_app_jo/urls.py
+++ my_app_jo/urls.py
@@ -9,27 +9,27 @@ from django.conf import settings
 from django.views.generic.base import RedirectView
 from my_app_jo.views import home
 
-urlpatterns = [
-    # path('', RedirectView.as_view(pattern_name='home', permanent=False)),
-    path('', home, name='home'),
-    path('choisir_ticket/', views.choisir_ticket, name='choisir_ticket'),
-    path('ajouter_au_panier/',views.ajouter_au_panier,name='ajouter_au_panier'),
-    path('voir_panier/', views.voir_panier, name='voir_panier'),
-    path('modifier_commande/<int:commande_id>/', views.modifier_commande, name='modifier_commande'),
-    path('payer_commande/<int:commande_id>/', views.payer_commande, name='payer_commande'),
-    path('supprimer_commande/<int:commande_id>/',views.supprimer_commande,name='supprimer_commande'),
-    path('mes_billets/', views.mes_billets, name='mes_billets'),
-    path('billet/<int:billet_id>/', views.details_billet, name='details_billet'),
-    path('connexion/', views.connexion, name='connexion'),
-    path('verificode',views.verificode,name='verificode'),
-    path('profile/', views.profile, name='profile'),
-    path('modifier_profile',views.modifier_profile,name='modifier_profile' ),
-    path('inscription/', views.inscription, name='inscription'),
-    path('deconnexion/', views.deconnexion, name='deconnexion'),
-    path('offres',views.offres,name='offres'),
+# urlpatterns = [
+#     # path('', RedirectView.as_view(pattern_name='home', permanent=False)),
+#     # path('', home, name='home'),
+#     path('choisir_ticket/', views.choisir_ticket, name='choisir_ticket'),
+#     path('ajouter_au_panier/',views.ajouter_au_panier,name='ajouter_au_panier'),
+#     path('voir_panier/', views.voir_panier, name='voir_panier'),
+#     path('modifier_commande/<int:commande_id>/', views.modifier_commande, name='modifier_commande'),
+#     path('payer_commande/<int:commande_id>/', views.payer_commande, name='payer_commande'),
+#     path('supprimer_commande/<int:commande_id>/',views.supprimer_commande,name='supprimer_commande'),
+#     path('mes_billets/', views.mes_billets, name='mes_billets'),
+#     path('billet/<int:billet_id>/', views.details_billet, name='details_billet'),
+#     path('connexion/', views.connexion, name='connexion'),
+#     path('verificode',views.verificode,name='verificode'),
+#     path('profile/', views.profile, name='profile'),
+#     path('modifier_profile',views.modifier_profile,name='modifier_profile' ),
+#     path('inscription/', views.inscription, name='inscription'),
+#     path('deconnexion/', views.deconnexion, name='deconnexion'),
+#     path('offres',views.offres,name='offres'),
 
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'my_app_jo.views.handler404'
+# handler404 = 'my_app_jo.views.handler404'
