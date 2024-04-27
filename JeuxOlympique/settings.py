9fc7a45e0e68bd384300e41a251ee5ecfc6bcc37
Bourama coulibaly <bouramadevweb@gmail.com>
2024-04-28 00:07:45 +0200

    changement venv

--- JeuxOlympique/settings.py
+++ JeuxOlympique/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 import os
+from pickle import FALSE
 import django_heroku
 import dj_database_url
 from  decouple import config
@@ -135,6 +136,16 @@ DATABASES = {
     "default": dj_database_url.parse(config('DATABASE_URL'))
 }
 
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_S3_SIGNATURE_NAME = 'S3V4'
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_REGION_NAME = config('AWS_REGION')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
 # Password validation
