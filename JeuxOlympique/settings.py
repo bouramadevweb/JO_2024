2da1a7e4a4c22d28b8eca3792a7902568755e6c1
Bourama coulibaly <bouramadevweb@gmail.com>
2024-05-28 18:41:26 +0200

    email env

--- JeuxOlympique/settings.py
+++ JeuxOlympique/settings.py
@@ -217,9 +217,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jeuxos2024@gmail.com'  
-EMAIL_HOST_PASSWORD = 'z j q u n k b d d s g b m u t a' 
-DEFAULT_FROM_EMAIL = 'jeuxos2024@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
 import os
