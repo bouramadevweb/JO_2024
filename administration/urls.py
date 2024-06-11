d5259d3b84d947790f033eae8e7828d0fd4f4325
Bourama coulibaly <bouramadevweb@gmail.com>
2024-06-11 15:14:01 +0200

    admin reset

--- administration/urls.py
+++ administration/urls.py
@@ -27,11 +27,11 @@ urlpatterns = [
     path('admin_profile/', views.admin_profile, name ='admin_profile'),
     path('admin_modifier_profile',views.admin_modifier_profile, name = 'admin_modifier_profile'),
 
-    path('admin/password_reset/', auth_views.PasswordResetView.as_view(template_name='admin/password_reset_form.html'), name='password_reset'),
+    # path('admin/password_reset/', auth_views.PasswordResetView.as_view(template_name='admin/password_reset_form.html'), name='password_reset'),
 
-    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='admin/password_reset_done.html'), name='password_reset_done'),
-    path('admin/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('admin/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='admin/password_reset_done.html'), name='password_reset_done'),
+    # path('admin/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('admin/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
