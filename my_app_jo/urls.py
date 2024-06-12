1233d656d811f176fa3153e32dd77fe44c476d80
Bourama coulibaly <bouramadevweb@gmail.com>
2024-06-12 21:38:09 +0200

    reset password user

--- my_app_jo/urls.py
+++ my_app_jo/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name ='password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
